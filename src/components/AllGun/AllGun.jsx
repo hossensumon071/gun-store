79a663d5f5f803b1c6d86870ea17eb0e91b94e05
hossensumon071 <55638643+hossensumon071@users.noreply.github.com>
2022-12-06 13:00:24 +0600

    single gun card ui complete

--- src/components/AllGun/AllGun.jsx
+++ src/components/AllGun/AllGun.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import SingleGun from '../SIngleGun/SingleGun';
+import './AllGun.css';
 
 const AllGun = () => {
+  const [guns, setGuns] = useState([]);
+  useEffect(()=>{
+    fetch(`https://raw.githubusercontent.com/mir-hussain/guns/main/data.json`)
+    .then(res => res.json())
+    .then(data => setGuns(data))
+  },[]);
+
   return (
-    <div>
-      
+    <div className='mt-5'>
+      <h1 className="text-4xl text-center font-bold mt-6">Welcome to <span className='gun-text'>Gun Store</span></h1>
+      <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-5">
+      {
+        guns.map((gun) => (
+          <SingleGun gun={gun} key={gun.id}/>
+        ))
+      }
+      </div>
     </div>
   );
 };
